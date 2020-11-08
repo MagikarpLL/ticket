c3269dbc2fa312de45c8431a2c45303a0219639f
MagikarpLL <magikarpllxd@gmail.com>
2020-11-08 20:31:44 +0800

    Initial commit

---
+++ login/src/main/java/cn/magikarpll/ticket/App.java
@@ -0,0 +1,13 @@
+package cn.magikarpll.ticket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
